b3edeb6146f47a47452ce526f2a27da71fbd8051
eng618 <eng618@fullsail.edu>
2014-01-23 07:33:02 -0500

    Created singleton for business object

---
+++ 1401MDF1-Project3/ENGBusList.h
@@ -0,0 +1,13 @@
+//
+//  ENGBusList.h
+//  1401MDF1-Project3
+//
+//  Created by Eric Garcia on 1/23/14.
+//  Copyright (c) 2014 Full Sail. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ENGBusList : NSObject
+
+@end
---
+++ 1401MDF1-Project3/ENGBusList.m
@@ -0,0 +1,13 @@
+//
+//  ENGBusList.m
+//  1401MDF1-Project3
+//
+//  Created by Eric Garcia on 1/23/14.
+//  Copyright (c) 2014 Full Sail. All rights reserved.
+//
+
+#import "ENGBusList.h"
+
+@implementation ENGBusList
+
+@end
